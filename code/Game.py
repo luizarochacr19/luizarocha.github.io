baa70eb78c0ea597b45428730e942ee2174b0282
LuizaRocha <luizarochacr@gmail.com>
2025-08-26 23:56:18 -0300

    Creating interactive menu

--- code/Game.py
+++ code/Game.py
@@ -1,6 +1,7 @@
 import pygame
 
-from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.Level import Level
 from code.Menu import Menu
 
 class Game:
@@ -11,11 +12,14 @@ class Game:
     def run(self, ):
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
 
-            # Check for all events
-           # for event in pygame.event.get():
-            #    if event.type == pygame.QUIT:
-             #       pygame.quit() # Close Window
-              #      quit() # End pygame
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1]]:
+                level = Level(self.window, 'Level 1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[3]:
+                pygame.quit()
+                quit()
+            else:
+                pass
+
---
+++ code/Level.py
@@ -0,0 +1,11 @@
+from code.Entity import Entity
+
+class Level:
+    def __init__(self, window, name, game_mode):
+        self.window = window
+        self.name = name
+        self.game_mode = game_mode #modo de jogo
+        self.entity_list = list[Entity] = []
+
+    def run(self, ):
+        pass
